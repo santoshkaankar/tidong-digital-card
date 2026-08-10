50c1b79929d1a0f2055b6380ecff6710298a9319
Santosh Kaankar <santoshkaankar@gmail.com>
2026-08-10 14:28:26 +0530

    add https force and trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Render / Cloudflare proxy ko trust karne ke liye
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
