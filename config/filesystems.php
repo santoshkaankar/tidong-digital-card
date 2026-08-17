a790d5a9a8fee3f7263d458b99228553926130b5
Santosh Kaankar <santoshkaankar@gmail.com>
2026-08-17 05:50:59 +0530

    image fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,19 @@ return [
             'report' => false,
         ],
 
+        // Yahan Supabase ka disk add kar diya gaya hai
+        'supabase' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'throw' => true,
+        ],
+
     ],
 
     /*
